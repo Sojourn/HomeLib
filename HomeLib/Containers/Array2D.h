099f64afd05bf24ce00faafa66b05428d3a9ba0b
Joel Rausch <jsrausch@iastate.edu>
2013-03-13 11:27:10 -0500

    Added default + copy constructors to Array2D
    Added a default and copy constructor to Array2D.
    
    The assignment operator has also been overloaded, and uses a new public
    function, resize to do this. The resize function destructively resizes
    the internal arrays. This is called from some of the constructor
    overloads, and the assignment operator.
    
    A private release function was added to free memory and invalidate the
    array. This is used when memory needs to be explicitly released during a
    resize operation.

--- HomeLib/Containers/Array2D.h
+++ HomeLib/Containers/Array2D.h
@@ -13,15 +13,25 @@ namespace Container
 	template<class T>
 	class Array2D
 	{
-		NO_COPY(Array2D)
-
 	public:
+
+		// Default constructor creates an invalid array. Call resize before use.
+		inline Array2D();
 
 		// Construct a 2d array with the given width and height.
 		inline Array2D(size_t width, size_t height);
 
+		// Copy construct from anouther 2d array.
+		inline Array2D(const Array2D<T> &other);
+
+		// Copy from anouther array. May resize this array.
+		inline const Array2D &operator=(const Array2D &other);
+
 		// Dtor.
 		inline ~Array2D(void);
+
+		// Destructively resize the 2d array.
+		inline void resize(size_t width, size_t height);
 
 		// Return the number of columns.
 		inline size_t width() const;
@@ -41,26 +51,101 @@ namespace Container
 		// Return a const reference to the element at the location. Bounds checked.
 		inline const T& at(size_t x, size_t y) const;
 
-		// Return a pointer to the y'th row.
+		// Return a pointer to the y'th row. No bounds checking.
 		inline T* operator[](size_t y);
 
-		// Return a const pointer to the y'th row.
+		// Return a const pointer to the y'th row. No bounds checking.
 		inline const T* operator[](size_t y) const;
 
 	private:
-		const size_t _width;
-		const size_t _height;
+
+		// Frees resources and invalidates the array.
+		void release();
+
+		size_t _width;
+		size_t _height;
 		T** _array;
 	};
 }
 
 template<class T>
+Container::Array2D<T>::Array2D() :
+	_width(0),
+	_height(0),
+	_array(nullptr)
+{
+}
+
+template<class T>
 Container::Array2D<T>::Array2D(size_t width, size_t height) :
-	_width(width),
-	_height(height)
+	_width(0),
+	_height(0),
+	_array(nullptr)
+{
+	resize(width, height);
+}
+
+template<class T>
+Container::Array2D<T>::Array2D(const Array2D<T> &other) :
+	_width(0),
+	_height(0),
+	_array(nullptr)
+{
+	if((other.width() != width()) || (other.height != height()))
+	{
+		resize(other.width(), other.height());
+	}
+
+	for(size_t y = 0; y < height(); y++)
+	{
+		for(size_t x = 0; x < width(); x++)
+		{
+			at(x, y) = other.at(x, y);
+		}
+	}
+}
+
+template<class T>
+const Container::Array2D<T> &Container::Array2D<T>::operator=(const Array2D<T> &other)
+{
+	if(this != &other)
+	{
+		if((other.width() != width()) || (other.height != height()))
+		{
+			resize(other.width(), other.height());
+		}
+
+		for(size_t y = 0; y < height(); y++)
+		{
+			for(size_t x = 0; x < width(); x++)
+			{
+				at(x, y) = other.at(x, y);
+			}
+		}
+	}
+
+	return *this;
+}
+
+template<class T>
+Container::Array2D<T>::~Array2D(void)
+{
+	release();
+}
+
+template<class T>
+void Container::Array2D<T>::resize(size_t width, size_t height)
 {
 	assert(width > 0);
 	assert(height > 0);
+
+	if(_array)
+	{
+		release();
+	}
+
+	_width = width;
+	_height = height;
 
 	_array = new T*[sizeof(T*) * _height];
 
@@ -69,13 +154,6 @@ Container::Array2D<T>::Array2D(size_t width, size_t height) :
 	{
 		_array[y] = &elements[y * _width]; 
 	}
-}
-
-template<class T>
-Container::Array2D<T>::~Array2D(void)
-{
-	delete [] _array[0];
-	delete [] _array;
 }
 
 template<class T>
@@ -107,6 +185,7 @@ T& Container::Array2D<T>::at(size_t x, size_t y)
 {
 	assert((x >= 0) && (x < width()));
 	assert((y >= 0) && (y < height()));
+	assert(_array != nullptr);
 	return (*this)[y][x];
 }
 
@@ -115,6 +194,7 @@ const T& Container::Array2D<T>::at(size_t x, size_t y) const
 {
 	assert((x >= 0) && (x < width()));
 	assert((y >= 0) && (y < height()));
+	assert(_array != nullptr);
 	return (*this)[y][x];
 }
 
@@ -128,6 +208,17 @@ template<class T>
 const T* Container::Array2D<T>::operator[](size_t y) const
 {
 	return _array[y];
+}
+
+template<class T>
+void Container::Array2D<T>::release()
+{
+	delete [] _array[0];
+	delete [] _array;
+
+	_width = 0;
+	_height = 0;
+	_array = nullptr;
 }
 
 #endif // ARRAY_H
--- HomeLib/Main.cpp
+++ HomeLib/Main.cpp
@@ -28,6 +28,17 @@ int main(int argc, char **argv)
 	doc.at(0, 0) = 7;
 	doc.at(63, 1023) = 9;
 
+	Container::Array2D<char> doc2(doc);
+	Container::Array2D<char> doc3(64, 64);
+	doc3 = doc2;
+
+	for(size_t y = 0; y < doc.height(); y++)
+	for(size_t x = 0; x < doc.width(); x++)
+		doc[y][x] = (y * x) % 127;
+	doc3.at(3, 4) = 6;
+	doc3.at(0, 0) = 7;
+	doc3.at(63, 1023) = 9;
+
 	std::system("pause");
 	return 0; 
 }
